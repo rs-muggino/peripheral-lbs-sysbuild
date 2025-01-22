8b40aa8fb89ea98c5292953b2be55ebd1ce233fd
rs-muggino <ryan@muggino.com>
2025-01-22 11:50:40 +1100

    able to partition using .yml

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # peripheral-lbs-sysbuild
+
+can currently get a partition table that looks good in theory, but building the project and getting it ready for flash has some strange error messages about gaps in the partition table
+
+ west build -b nrf52dk/nrf52832 --sysbuild -d new_build_sysbuild . -t partition_manager_report
---
+++ new_build_sysbuild/peripheral_lbs/zephyr/include/generated/app_commit.h
@@ -0,0 +1,12 @@
+#ifndef _APP_COMMIT_H_
+#define _APP_COMMIT_H_
+
+/*  values come from cmake/version.cmake
+ * BUILD_COMMIT related  values will be 'git rev-parse',
+ * alternatively user defined BUILD_VERSION.
+ */
+
+#define APP_COMMIT                   8587b81e4e03
+#define APP_COMMIT_STRING            "8587b81e4e03"
+
+#endif /* _APP_COMMIT_H_ */
